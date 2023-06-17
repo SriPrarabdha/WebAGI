2c94f56493b11cc1cb4fe5498d6a2e76f8f196db
Prarabdha <prabodhkumarsrivastav70@gmail.com>
2023-06-17 16:15:19 +0530

    lawGPT agent

--- src/agents/common/tools/criminalLaw.ts
+++ src/agents/common/tools/criminalLaw.ts
@@ -1,3 +1,18 @@
-import { getUserApiKey } from '@/utils/settings';
-import { OpenAIChat } from 'langchain/llms/openai';
+// import { getUserApiKey } from '@/utils/settings';
+// import { OpenAIChat } from 'langchain/llms/openai';
 // import {PineconeHybridSearchRetriever} from 'langchain/retrievers'
+
+
+const lawGPT = async (question: string) => {
+    const url = "https://langchain-e2c6684ec1.wolf.jina.ai/"+question
+    try {
+      const response = await fetch(url);
+      console.log(response.json);
+      return response.json;
+    } catch (error) {
+      console.error(`Error while fetching the URL: ${error}`);
+      return '';
+    }
+  };
+
+export default lawGPT;
