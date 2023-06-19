4a3aeaf325cd43e1f866305ff3e3ff5f7272c8cb
Himanshu <himanshu_co21a4_27@dtu.ac.in>
2023-06-19 14:40:03 +0530

    Update OutputMsg.tsx

--- src/components/Agent/OutputMsg.tsx
+++ src/components/Agent/OutputMsg.tsx
@@ -7,8 +7,7 @@ export interface props {
 }
 
 export const OutputMsg: FC<props> = ({ message }) => {
-  const [count, setCount] = useState(0);
-
+  
   return (
     <div className="relative m-auto flex gap-4 px-4 text-base md:max-w-2xl md:gap-6 lg:max-w-2xl lg:px-0 xl:max-w-3xl">
       <h2 className='text-gray-800 '>{message}</h2>
